8a7917c6da0cf1f254deb9baa22eee869c689b6e
AFRANIO DIMAS DA SILVA <86030954+AfranioD@users.noreply.github.com>
2022-08-21 10:59:05 -0300

    Create iac.sh

---
+++ iac.sh
@@ -0,0 +1,41 @@
+#!./bin/bash
+
+echo "Diretórios"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Grupos de usários"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários"
+
+useradd calos -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+useraddroberto -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Definindo permissões"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Finalizado"
