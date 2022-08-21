501aa45e4660a63173679bdf01f3edc678a7ef3b
AFRANIO DIMAS DA SILVA <86030954+AfranioD@users.noreply.github.com>
2022-08-21 12:13:18 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Criando usuários"
 
-useradd calos -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_ADM
+useradd calos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
-useraddroberto -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useraddroberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $ (openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
 echo "Definindo permissões"
 
